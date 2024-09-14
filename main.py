683f226abb6050495cb296b54621ce6221528b28
joacoembon <jembon@fi.uba.ar>
2024-09-14 19:45:30 -0300

    Pre-algoritmo O(n^2)

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import sys
 import n_monedas
 
-sys.setrecursionlimit(100000)
-
 def main():
-    ruta = "pruebas/20000.txt"     #  sys.argv[:]
+    ruta = input("Ingrese ruta:")
     monedas = n_monedas.cargar_monedas_archivo(ruta)
-    print(n_monedas.juego_sophia_mateo(monedas))
-    #return
+    n_monedas.juego_sophia_mateo(monedas)
+
 main()
--- n_monedas.py
+++ n_monedas.py
@@ -1,7 +1,5 @@
 import sys
 
-sys.setrecursionlimit(100000)
-
 def cargar_monedas_archivo(ruta):
     monedas = []
     with open(ruta) as file:
@@ -16,26 +14,27 @@ def juego_sophia_mateo(monedas):
     monedas_sophia = []
     monedas_mateo = []
     es_turno_de_sophia = True
-
-    while monedas:
+    
+    while len(monedas) > 0:
         if es_turno_de_sophia:
             moneda = monedas.pop(posicion_moneda(monedas, maximo=True))
             monedas_sophia.append(moneda)
         else:
             moneda = monedas.pop(posicion_moneda(monedas, maximo=False))
             monedas_mateo.append(moneda)
+        
         es_turno_de_sophia = not es_turno_de_sophia
-
+    
     sumatoria_sophia = sum(monedas_sophia)
     sumatoria_mateo = sum(monedas_mateo)
-    return sumatoria_sophia
+    return sumatoria_sophia > sumatoria_mateo
 
 def posicion_moneda(monedas, maximo):
     if maximo:
-        if monedas[0] >= monedas[len(monedas) - 1]:
+        if monedas[0] >= monedas[-1]:
             return 0
         return len(monedas) - 1
     else:
-        if monedas[0] < monedas[len(monedas) - 1]:
+        if monedas[0] < monedas[-1]:
             return 0
         return len(monedas) - 1
