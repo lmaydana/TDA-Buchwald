617f6b1a8577e99348019478d88c518ed7008102
lmaydana <lmaydana@fi.uba.ar>
2024-09-27 12:01:51 -0300

    Modificando los archivos para que muestren mas informacion de la partida

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import n_monedas
 def main():
     ruta = input("Ingrese ruta:")
     monedas = n_monedas.cargar_monedas_archivo(ruta)
-    print(f"¿Ganó de Sophia?: {n_monedas.juego_sophia_mateo(monedas)}")
+    monedas_sophia, monedas_mateo = n_monedas.juego_sophia_mateo(monedas)
+    print(n_monedas.devolver_jugadas(monedas_sophia, monedas_mateo))
+    ganancia_sophia = n_monedas.devolver_ganancia(monedas, monedas_sophia)
+    ganancia_mateo = n_monedas.devolver_ganancia(monedas, monedas_mateo)
+    print(f"Ganancia Sophia: {ganancia_sophia}")
+    print(f"Ganancia Mateo: {ganancia_mateo}")
+    print(f"¿Ganó Sophia?: {ganancia_sophia > ganancia_mateo}")
 
 main()
