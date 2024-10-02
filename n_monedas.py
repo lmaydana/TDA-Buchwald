df17de0ed3d5cbdfd3d70981ad967ef683d24c96
Benjamin <benjacb22@hotmail.com>
2024-10-02 14:35:50 -0300

    Algun detalle del codigo

--- n_monedas.py
+++ n_monedas.py
@@ -39,32 +39,3 @@ def posicion_moneda(monedas, ini, fin, maximo):
         if monedas[ini] < monedas[fin]:
             return ini
         return fin
-def devolver_ganancia(monedas, monedas_jugador):
-    ganancia = 0
-    for indice in monedas_jugador:
-        ganancia += monedas[indice]
-    return ganancia
-
-def devolver_jugadas(monedas_sophia, monedas_mateo):
-    juego = ""
-    primera = 0
-    ultima = len(monedas_sophia) + len(monedas_mateo) - 1
-    for i in range(len(monedas_mateo)):
-        moneda_sophia = monedas_sophia[i]
-        moneda_mateo = monedas_mateo[i]
-        elegida_mateo = "Última"
-        elegida_sophia = "Última"
-        if moneda_sophia == primera:
-            elegida_sophia = "Primera"
-            primera += 1
-        else:
-            ultima -= 1
-        if moneda_mateo == primera:
-            elegida_mateo = "Primera"
-            primera +=1
-        else:
-            ultima -= 1
-        juego+=f"{elegida_sophia} moneda para Sophia; {elegida_mateo} moneda para Mateo; "
-    if len(monedas_sophia) > len(monedas_mateo):
-        juego+=f"Última moneda para Sophia"
-    return juego
