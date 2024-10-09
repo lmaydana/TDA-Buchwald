3d4e4153c26a7b49e07ea36e0dd167d01dd0cc82
Benjamin <benjacb22@hotmail.com>
2024-10-09 14:36:53 -0300

    Un detalle de codigo

--- n_monedas.py
+++ n_monedas.py
@@ -16,17 +16,24 @@ def juego_sophia_mateo(monedas):
     es_turno_de_sophia = True
     ini = 0
     fin = len(monedas) - 1
+    
     while ini <= fin:
-        monedas_jugador = monedas_sophia
-        posicion_moneda_a_agregar = posicion_moneda(monedas, ini, fin, maximo=es_turno_de_sophia)
-        if not es_turno_de_sophia:
-            monedas_jugador = monedas_mateo
-        monedas_jugador.append(posicion_moneda_a_agregar)
-        if posicion_moneda_a_agregar == fin:
-            fin -= 1
+        if es_turno_de_sophia:
+            posicion_moneda_a_agregar = posicion_moneda(monedas, ini, fin, maximo=True)
+            monedas_sophia.append(monedas[posicion_moneda_a_agregar])
+            if posicion_moneda_a_agregar == fin:
+                fin -= 1
+            else:
+                ini += 1
         else:
-            ini += 1
+            posicion_moneda_a_agregar = posicion_moneda(monedas, ini, fin, maximo=False)
+            monedas_mateo.append(monedas[posicion_moneda_a_agregar])
+            if posicion_moneda_a_agregar == fin:
+                fin -= 1
+            else:
+                ini += 1
         es_turno_de_sophia = not es_turno_de_sophia
+
 
     return monedas_sophia, monedas_mateo
 
