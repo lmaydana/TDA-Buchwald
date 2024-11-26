5607651d6382aee6806bb016c7145061687f774f
joacoembon <jembon@fi.uba.ar>
2024-11-26 13:48:46 -0300

    Cambios en el verificador eficiente para que corobore todo lo pedido (largo de barcos, cantidad de barcos, adyacencia de barcos, demandas de filas y columnas.

--- verificador_eficiente.py
+++ verificador_eficiente.py
@@ -1,123 +1,110 @@
-NUMERO_BARCO = 0
-POSICION_EXTREMO_INICIAL = 1
-POSICION_EXTREMO_FINAL = 2
-FILA = 0
-COLUMNA = 1
+"""
+La idea de un verificador eficiente debe ser que dada una solucion a un problema, se corobore de manera polinomial
+si esa solucion es valida o no.
+Para este caso tendremos en cuenta la consigna de:
+Dado un tablero de nxm casilleros, y una lista de k barcos (donde el barco i tiene bi de largo), una lista de 
+restricciones para las filas (donde la restricción j corresponde a la cantidad de casilleros a ser ocupados en 
+la fila j) y una lista de restricciones para las columnas (símil filas, pero para columnas), ¿es posible definir 
+una ubicación de dichos barcos de tal forma que se cumplan con las demandas de cada fila y columna, y las 
+restricciones de ubicación?
+"""
 
-def verificador_eficiente(tablero, barcos, demanda_fila, demanda_columna):
-	solucion = []
-	barcos_colocados = {}
+def verificador_eficiente(tablero_solucion, barcos, demandas_filas, demandas_columnas):
+    """
+    Pre: Recibe:
+    - Un tablero con los barcos ubicados como solucion (siendo 0 espacios libres y numeros los barcos).
+    - Lista de barcos original (cada indice i deberia ser el largo del barco i+1).
+    - Lista de demandas de filas original.
+    - Lista de demandas de columnas original.
 
-	for fila_pos in range(len(tablero)):
-		fila = tablero[fila_pos]
-		for celda_pos in range(len(fila)):
-			celda = fila[celda_pos]
-			if celda != 0:
-				barcos_colocados[celda] = barcos_colocados.get(celda, []) + [(fila_pos, celda_pos)]
-	barcos_copia = barcos.copy()
-	for clave in barcos_colocados:
-		for posicion_inicial in range(len(barcos_colocados[clave])):
-			pos_extremo_inicial = barcos_colocados[clave][posicion_inicial]
-			for posicion_final in range((posicion_inicial + 1, len(barcos_colocados[clave]))):
-				pos_extremo_final = barcos_colocados[clave][posicion_final]
-				diferencia_fila = pos_extremo_final[FILA] - pos_extremo_inicial[FILA]
-				diferencia_columna = pos_extremo_final[COLUMNA] - pos_extremo_inicial[COLUMNA]
-				if diferencia_fila != 0 and diferencia_columna != 0:
-					return False
-		pos_extremo_inicial = barcos_colocados[clave][0]
-		pos_extremo_final = barcos_colocados[clave][-1]
-		diferencia_fila_total = pos_extremo_final[FILA] - pos_extremo_inicial[FILA]
-		diferencia_columna_total = pos_extremo_final[COLUMNA] - pos_extremo_inicial[COLUMNA]
-		largo_barco = max(diferencia_fila_total, diferencia_columna_total) + 1
-		if largo_barco != barcos[clave - 1]:
-			return False
-		if !es_el_unico_en_radio(tablero, pos_extremo_inicial[FILA], pos_extremo_inicial[COLUMNA], largo_barco, clave, diferencia_columna_total!=0):
-			return False
-		solucion.append((clave - 1, barcos_colocados[clave][0], barcos_colocados[clave][-1]))
+    Debemos validar que la solucion:
+    - Cubra todas las demandas de filas y columnas (ni mas ni menos).
+    - Los barcos sean los mismos que los de la lista original de barcos.
+    - Los barcos cumplan con la ley de adyacencia.
+    - No hayan barcos de mas.
 
+    Pos: 
+    - Devuelve True en caso de que sea una solucion valida y False en caso contrario.
+    """
 
-	return verificar_solucion(barcos_copia, demanda_fila, demanda_columna, solucion)
+    demanda_restante = calcular_demandas_restantes(tablero_solucion, demandas_filas, demandas_columnas)
+    if demanda_restante != 0:
+        return False
+    
+    n, m = tablero_solucion.shape
+    barcos_checkeados = set()
 
-def es_el_unico_en_radio(tablero, fila, columna, largo_barco, clave, orientacion_fila):
-	suma_sector = 0
-	if orientacion_fila:
-		for fila_sector in range(max(fila - 1, 0), min(fila + 2, len(tablero))):
-			for columna_sector in range(max(columna - 1, 0), min(columna + largo_barco + 1, len(tablero[fila]))):
-				suma_sector += tablero[fila_sector][columna_sector]
-	else:
-		for fila_sector in range(max(fila - 1, 0), min(fila + largo_barco + 1, len(tablero))):
-			for columna_sector in range(max(columna - 1, 0), min(columna + 2, len(tablero[fila]))):
-				suma_sector += tablero[fila_sector][columna_sector]
-	return suma_sector == clave*largo_barco
+    for fila in range(len(tablero_solucion)):
+        for columna in range(len(tablero_solucion[0])):
+            if tablero_solucion[fila][columna] != 0 and not tablero_solucion[fila][columna] in barcos_checkeados:
+                barco = tablero_solucion[fila][columna]
+                
+                if barco < len(barcos):
+                    # Si esta en el rango de barcos, sera el largo que deberia tener originalmente.
+                    barco_largo = barcos[barco-1]
+                else: 
+                    return False
 
-
-
-def verificar_solucion(barcos, demanda_fila, demanda_columna, solucion):
-	casilleros_ocupados = {}
-	demanda_fila_incumplida = demanda_fila.copy()
-	demanda_columna_incumplida = demanda_columna.copy()
-	for barco_posicion in solucion:
-		pos_extremo_inicial = barco_posicion[POSICION_EXTREMO_INICIAL]
-		pos_extremo_final = barco_posicion[POSICION_EXTREMO_FINAL]
-		diferencia_fila = pos_extremo_final[FILA] - pos_extremo_inicial[FILA]
-		diferencia_columna = pos_extremo_final[COLUMNA] - pos_extremo_inicial[COLUMNA]
-		if not estan_todos_los_casilleros_disponibles(pos_extremo_inicial, pos_extremo_final, casilleros_ocupados):
-			return False
-		largo_barco = barcos[barco_posicion[NUMERO_BARCO]]
-		orientacion_barco_perpendicular = FILA
-		orientacion_barco = COLUMNA
-		demanda_orientacion = demanda_columna_incumplida
-		demanda_orientacion_perpendicular = demanda_fila_incumplida
-		if diferencia_columna > 0:
-			orientacion_barco_perpendicular = COLUMNA
-			orientacion_barco = FILA
-			demanda_orientacion = demanda_fila_incumplida
-			demanda_orientacion_perpendicular = demanda_columna_incumplida
-		demanda_orientacion[pos_extremo_inicial[orientacion_barco]] -= largo_barco
-		for avance in range(min(pos_extremo_inicial[orientacion_barco_perpendicular], pos_extremo_final[orientacion_barco_perpendicular]), max(pos_extremo_inicial[orientacion_barco_perpendicular], pos_extremo_final[orientacion_barco_perpendicular]) + 1):
-			demanda_orientacion_perpendicular[avance] -= 1
-			posicion_ocupada = (pos_extremo_inicial[FILA], avance)
-			if diferencia_fila > 0:
-				posicion_ocupada = (avance, pos_extremo_inicial[COLUMNA])
-			casilleros_ocupados[posicion_ocupada] = True
-	for demanda in demanda_fila_incumplida:
-		if demanda != 0:
-		    return False
-			
-	for demanda in demanda_columna_incumplida:
-		if demanda != 0:
-			return False
-	
-	return True
+                if barco > 1:
+                    # Si el barco no es de un solo bloque de largo.
+                    if columna < m and tablero_solucion[fila][columna + 1] == barco:
+                        if not se_puede_colocar(tablero_solucion, barco, barco_largo, fila, columna, "H"):
+                            return False
+                        
+                    if fila < n and tablero_solucion[fila + 1][columna] == barco:
+                        if not se_puede_colocar(tablero_solucion, barco, barco_largo, fila, columna, "V"):
+                            return False
+                        
+                else:
+                    if not se_puede_colocar(tablero_solucion, barco, barco_largo, fila, columna, None):
+                        return False
+                    
+                barcos_checkeados.add(barco)
+    
+    return True
 
 
+def calcular_demandas_restantes(tablero, demandas_filas, demandas_columnas):
+    """
+    Calcula cuanta demanda falta por cubrir en cada fila y columna.
+    """
+    demandas_restantes_filas = demandas_filas.copy()
+    demandas_restantes_columnas = demandas_columnas.copy()
+    
+    for fila in range(len(tablero)):
+        for columna in range(len(tablero[0])):
+            if tablero[fila][columna] != 0:  # Hay un barco en esta celda.
+                demandas_restantes_filas[fila] = demandas_restantes_filas[fila] - 1
+                demandas_restantes_columnas[columna] = demandas_restantes_columnas[columna] - 1
 
-def estan_todos_los_casilleros_disponibles(pos_extremo_inicial, pos_extremo_final, casilleros_ocupados):
-	direccion = obtener_direccion(pos_extremo_inicial, pos_extremo_final)
-	if direccion == (0,0):
-		return not (pos_extremo_inicial in casilleros_ocupados)
-
-	disposicion_avance = FILA
-	if direccion[COLUMNA] > 0:
-		disposicion_avance = COLUMNA
-
-	pos_avance_inicial = pos_extremo_inicial[disposicion_avance]
-	pos_avance_final = pos_extremo_final[disposicion_avance]
-	direccion_avance = direccion[disposicion_avance]
-	for avance in range(pos_avance_inicial, pos_avance_final + direccion_avance, direccion_avance):
-		if disposicion_avance == FILA and (avance, pos_extremo_inicial[COLUMNA]) in casilleros_ocupados:
-			return False
-		elif disposicion_avance == COLUMNA and (pos_extremo_inicial[FILA], avance) in casilleros_ocupados:
-			return False
-	return True
+    return demandas_restantes_filas, demandas_restantes_columnas
 
 
-def obtener_direccion(pos_extremo_inicial, pos_extremo_final):
-	diferencia_fila = pos_extremo_final[FILA] - pos_extremo_inicial[FILA]
-	diferencia_columna = pos_extremo_final[COLUMNA] - pos_extremo_inicial[COLUMNA]
-	if diferencia_fila == 0 and diferencia_columna == 0:
-		return (0, 0)
-	elif diferencia_fila == 0:
-		return (0, int(diferencia_columna/abs(diferencia_columna)))
-	else:
-		return (int(diferencia_fila/abs(diferencia_fila)), 0)
+def se_puede_colocar(tablero, barco, barco_largo, fila, columna, orientacion):
+
+    filas, columnas = tablero.shape
+
+    if orientacion == 'H':  # Horizontal.
+        # Verificar hacia la derecha.
+        if columna + barco_largo <= columnas:
+            if all(tablero[fila, columna:columna + barco_largo] == barco):
+                # Verificar largo.
+                if all(tablero[max(0, fila - 1):min(filas, fila + 2), max(0, columna - 1):min(columnas, columna + barco + 1)].flatten() == 0):
+                    # Verificar adyacencia. (Si el largo es mayor al que deberia el barco, falla aca).
+                    return True
+
+    elif orientacion == 'V':  # Vertical.
+        # Verificar hacia abajo.
+        if fila + barco_largo <= filas:
+            if all(tablero[fila:fila + barco_largo, columna] == barco):
+                # Verificar largo.
+                if all(tablero[max(0, fila - 1):min(filas, fila + barco_largo + 1), max(0, columna - 1):min(columnas, columna + 2)].flatten() == 0):
+                    # Verificar adyacencia.
+                    return True
+    
+    elif not orientacion:
+        if all(tablero[max(0, fila - 1):min(filas, fila + 2), max(0, columna - 1):min(columnas, columna + 2)].flatten() == 0):
+            # Verificar adyacencia.
+            return True
+
+    return False
