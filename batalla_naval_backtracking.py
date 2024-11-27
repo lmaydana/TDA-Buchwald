c481fca307ef486e4e52cf7afec314eb95a22999
joacoembon <jembon@fi.uba.ar>
2024-11-26 21:45:57 -0300

    Backtracking.

--- batalla_naval_backtracking.py
+++ batalla_naval_backtracking.py
@@ -2,44 +2,39 @@ import numpy as np
 
 def puede_colocar_barco(tablero, barco, fila, columna, orientacion, demandas_filas, demandas_columnas):
     """
-    Verifica si es posible colocar un barco en cualquier dirección (derecha/izquierda o abajo/arriba)
-    respetando las restricciones de:
+    Verifica si es posible colocar un barco hacia abajo si es con orientacion "vertical" o hacia 
+    derecha si es con orientacion "horizontal" respetando las restricciones de:
     - Adyacencia.
     - Demandas de filas y columnas.
     """
-    tablero_cop = tablero.copy()
-    demandas_filas_cop = demandas_filas.copy()
-    demandas_columnas_cop = demandas_columnas.copy()
     filas, columnas = tablero.shape
 
     if orientacion == 'H':  # Horizontal
         # Verificar hacia la derecha
         if columna + barco <= columnas:
-            if all(tablero_cop[fila, columna:columna + barco] == 0) and \
-                    demandas_filas_cop[fila] - barco >= 0 and \
-                    all(demandas_columnas_cop[j] - 1 >= 0 for j in range(columna, columna + barco)):
+            if all(tablero[fila, columna:columna + barco] == 0) and \
+                    demandas_filas[fila] - barco >= 0 and \
+                    all(demandas_columnas[j] - 1 >= 0 for j in range(columna, columna + barco)):
                 # Verificar adyacencia
-                if all(tablero_cop[max(0, fila - 1):min(filas, fila + 2), max(0, columna - 1):min(columnas, columna + barco + 1)].flatten() == 0):
+                if all(tablero[max(0, fila - 1):min(filas, fila + 2), max(0, columna - 1):min(columnas, columna + barco + 1)].flatten() == 0):
                     return True
-
 
     elif orientacion == 'V':  # Vertical
         # Verificar hacia abajo
         if fila + barco <= filas:
-            if all(tablero_cop[fila:fila + barco, columna] == 0) and \
-                    demandas_columnas_cop[columna] - barco >= 0 and \
-                    all(demandas_filas_cop[i] - 1 >= 0 for i in range(fila, fila + barco)):
+            if all(tablero[fila:fila + barco, columna] == 0) and \
+                    demandas_columnas[columna] - barco >= 0 and \
+                    all(demandas_filas[i] - 1 >= 0 for i in range(fila, fila + barco)):
                 # Verificar adyacencia
-                if all(tablero_cop[max(0, fila - 1):min(filas, fila + barco + 1), max(0, columna - 1):min(columnas, columna + 2)].flatten() == 0):
+                if all(tablero[max(0, fila - 1):min(filas, fila + barco + 1), max(0, columna - 1):min(columnas, columna + 2)].flatten() == 0):
                     return True
-
 
     return False
 
 
 def colocar_barco(tablero, barco, fila, columna, orientacion, id_barco, demandas_fila, demandas_columna):
     """
-    Coloca o retira un barco en el tablero, eligiendo la direccion.
+    Coloca o retira un barco en el tablero, eligiendo la direccion y actualiza demandas.
     - Si es horizontal ('H'), elige extenderse a la derecha.
     - Si es vertical ('V'), elige extenderse hacia abajo.
     """
@@ -68,16 +63,13 @@ def colocar_barco(tablero, barco, fila, columna, orientacion, id_barco, demandas
             demandas_columna[columna] += barco
 
 
-def obtener_filas_columnas_prioritarias(demandas_filas, demandas_columnas, tablero):
+def obtener_filas_columnas_prioritarias(demandas_filas, demandas_columnas):
     """
     Obtiene listas de filas y columnas priorizadas por demanda restante absoluta (mayor urgencia).
     """
     # Priorizar por la demanda restante absoluta.
     filas_prioritarias = np.argsort(-demandas_filas).tolist() # Devuelve los indices.
     columnas_prioritarias = np.argsort(-demandas_columnas).tolist()
-
-    print(f"Filas prioritarias: {filas_prioritarias}")
-    print(f"Columnas prioritarias: {columnas_prioritarias}")
 
     return filas_prioritarias, columnas_prioritarias
 
@@ -105,19 +97,20 @@ def batalla_naval_bt(tablero, barcos, demandas_filas, demandas_columnas, indice,
         sol_optima[0] = sol_parcial[0]
         sol_optima[1] = sol_parcial[1].copy()
 
-    if demandas_filas.sum() + demandas_columnas.sum() - (sum(barcos[indice:])*2) >= sol_optima[0]:
-        return
-
     barco = barcos[indice]
     id_barco = indice + 1  # Identificador unico para el barco.
 
-    # Obtener filas y columnas priorizadas.
-    filas_prioritarias, columnas_prioritarias = obtener_filas_columnas_prioritarias(demandas_filas, demandas_columnas, tablero.copy())
-
     cota_superior = sum(max(0, d - barco) for d in demandas_filas) + sum(max(0, d - barco) for d in demandas_columnas)
-    """Esto seria lo que mas podria abarcar el barco, dejando la sumatoria de demandas restastenes en fila y columna"""
     if cota_superior >= sol_optima[0]:
         return
+
+    if demandas_filas.sum() + demandas_columnas.sum() - (sum(barcos[indice:])*2) >= sol_optima[0]:
+        return
+    
+    filas_prioritarias, columnas_prioritarias = obtener_filas_columnas_prioritarias(demandas_filas, demandas_columnas)
+
+    if barco > max(demandas_filas[filas_prioritarias[0]], demandas_columnas[columnas_prioritarias[0]]):
+        return batalla_naval_bt(tablero, barcos, demandas_filas, demandas_columnas, indice + 1, sol_parcial, sol_optima)
 
     # Probar todas las combinaciones de posicion y orientacion.
     for fila in filas_prioritarias:
