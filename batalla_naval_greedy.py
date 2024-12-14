5c4e86e42c0c9477129b8fe1ed3fb8ddbfa6197e
lmaydana <lmaydana@fi.uba.ar>
2024-12-14 20:24:06 -0300

    Mejorando complejidad Jellicoe

--- batalla_naval_greedy.py
+++ batalla_naval_greedy.py
@@ -18,47 +18,50 @@ def batalla_naval(tablero, barcos, demandas_filas, demandas_columnas):
 		info_demanda = heapq._heappop_max(demandas_totales)
 		tipo_demanda = info_demanda[TIPO_DEMANDA]
 		pos_demanda = info_demanda[POSICION_DEMANDA]
-		casilleros_a_probar = len(demandas_filas) if tipo_demanda == "c" else len(demandas_columnas)
 		for indice_barco in indices_disponibles:
 			info_barco = barcos_aux[indice_barco]
 			barco = info_barco[LARGO_BARCO]
-			for celda in range(casilleros_a_probar - barco + 1):
-				fila_a_probar = pos_demanda if tipo_demanda == "f" else celda
-				columna_a_probar = pos_demanda if tipo_demanda == "c" else celda
-				demandas = demandas_columnas_pos if tipo_demanda == "c" else demandas_filas_pos
-				demandas_perpendiculares = demandas_filas_pos if tipo_demanda == "c" else demandas_columnas_pos
-				if entra_barco(barco, tablero, fila_a_probar, columna_a_probar, tipo_demanda, pos_demanda, demandas, demandas_perpendiculares):
-					colocar_barco(info_barco, tablero, tipo_demanda, fila_a_probar, columna_a_probar)
-					info_demanda[CANTIDAD_DEMANDA] -= barco
-					reducir_demandas_perpendiculares(celda, barco, demandas_perpendiculares)
-					heapq.heappush(demandas_totales, info_demanda)
-					heapq._heapify_max(demandas_totales)
-					indices_disponibles.pop(indice_barco)
-					break
-			if indice_barco not in indices_disponibles:
+			demandas = demandas_columnas_pos if tipo_demanda == "c" else demandas_filas_pos
+			demandas_perpendiculares = demandas_filas_pos if tipo_demanda == "c" else demandas_columnas_pos
+			celda = obtener_posicion(barco, tablero, pos_demanda, tipo_demanda, demandas, demandas_perpendiculares)
+			fila_a_probar = pos_demanda if tipo_demanda == "f" else celda
+			columna_a_probar = pos_demanda if tipo_demanda == "c" else celda
+			if celda != -1:
+				colocar_barco(info_barco, tablero, tipo_demanda, fila_a_probar, columna_a_probar)
+				info_demanda[CANTIDAD_DEMANDA] -= barco
+				reducir_demandas_perpendiculares(celda, barco, demandas_perpendiculares)
+				heapq.heappush(demandas_totales, info_demanda)
+				heapq._heapify_max(demandas_totales)
+				indices_disponibles.pop(indice_barco)
 				break
-			indice_barco += 1
 
 	return (sum(demandas_filas) + sum(demandas_columnas) - devolver_demanda_total_cumplida(demandas_filas_pos) - devolver_demanda_total_cumplida(demandas_columnas_pos)), tablero
 
-def entra_barco(barco, tablero, fila_a_probar, columna_a_probar, tipo_demanda, pos_demanda, demandas, demandas_perpendiculares):
+def obtener_posicion(barco, tablero, pos_demanda, tipo_demanda, demandas, demandas_perpendiculares):
+
 	if demandas[pos_demanda][CANTIDAD_DEMANDA] < barco:
-		return False
-	if tipo_demanda == "c" and fila_a_probar + barco > len(demandas_perpendiculares) or tipo_demanda == "f" and columna_a_probar + barco > len(demandas_perpendiculares):
-		return False
-	if tipo_demanda == "c" and not np.all(tablero[max(fila_a_probar - 1, 0): min(fila_a_probar + barco + 1, len(demandas_perpendiculares)), max(columna_a_probar - 1, 0): min(columna_a_probar + 2, len(demandas))] == 0) or tipo_demanda == "f" and not np.all(tablero[max(0, fila_a_probar - 1): min(fila_a_probar + 2, len(demandas)), max(0, columna_a_probar - 1): min(columna_a_probar + barco + 1, len(demandas_perpendiculares))] == 0):
-		return False
-
-	if tipo_demanda == "c" and not son_todas_las_demandas_contiguas_positivas(fila_a_probar, barco, demandas_perpendiculares) or tipo_demanda == "f" and not son_todas_las_demandas_contiguas_positivas(columna_a_probar, barco, demandas_perpendiculares):
-		return False
-	
-	return True
-
-def son_todas_las_demandas_contiguas_positivas(posicion, barco, demandas):
-	for pos in range(posicion, min(posicion + barco, len(demandas))):
-		if demandas[pos][CANTIDAD_DEMANDA] <= 0:
-			return False
-	return True
+		return -1
+	contiguos_disponibles = 0
+	posicion_contiguos = -1
+	for posicion_a_probar in range(len(demandas_perpendiculares)):
+		if posicion_contiguos + barco > len(demandas_perpendiculares):
+			return -1
+		posicion_fila_inicial = (posicion_a_probar if tipo_demanda == "c" else pos_demanda) - 1
+		posicion_fila_final = (posicion_a_probar if tipo_demanda == "c" else pos_demanda) + 2
+		posicion_columna_inicial = (pos_demanda if tipo_demanda == "c" else posicion_a_probar) - 1
+		posicion_columna_final = (pos_demanda if tipo_demanda == "c" else posicion_a_probar) + 2
+		limite_filas = len(demandas) if tipo_demanda == "f" else len(demandas_perpendiculares)
+		limite_columnas = len(demandas_perpendiculares) if tipo_demanda == "f" else len(demandas)
+		if np.all(tablero[max(posicion_fila_inicial, 0): min(posicion_fila_final, limite_filas), max(posicion_columna_inicial, 0): min(posicion_columna_final,limite_columnas)] == 0) and demandas_perpendiculares[posicion_a_probar][CANTIDAD_DEMANDA] > 0:
+			if contiguos_disponibles == 0:
+				posicion_contiguos = posicion_a_probar
+			contiguos_disponibles += 1
+		else:
+			posicion_contiguos = -1
+			contiguos_disponibles = 0
+		if contiguos_disponibles == barco:
+			return posicion_contiguos
+	return -1
 
 def colocar_barco(info_barco, tablero, tipo_demanda, fila_a_probar, columna_a_probar):
 	posicion_a_probar = fila_a_probar if tipo_demanda == "c" else columna_a_probar
